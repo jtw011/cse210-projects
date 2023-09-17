e86c94bcf32751be1d8b8f85cf368eb57a7ac748
jtw011 <wil20046@byui.edu>
2023-09-16 18:13:18 -0600

    finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Runtime;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+        letter = "A";
+        }
+
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        
+        else if (percent >=70)
+        {
+            letter = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+        letter = "f";
+        }
+
+        Console.WriteLine($"Your Grade is {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Better luck next semester!");
+        }
     }
 }
